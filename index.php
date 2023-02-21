08d3d3d686f85af8f10a531f8f51e936d091e673
Gass <madebygass@gmail.com>
2023-02-21 11:48:44 +0100

    homepage

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<?php 
-    require 'header.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="patisserie">
+            <img src="./asset/cheesecake.png" alt="cheesecake image">
+            <a href="patisseries.php">Pâtisseries</a>
+        </div>
+        <div class="viennoiserie">
+            <div class="logo">
+                <img src="./asset/logo_bakery.png" alt="logo">
+            </div>
+            <div class="content_viennoiserie">
+                <img src="./asset/croissant.png" alt="image croissant">
+                <a href="vienoiseries.php">Viennoiserie</a>
+            </div>
+        </div>
+        <div class="boisson">
+            <img src="./asset/soda.png" alt="logo-boisson">
+            <a href="boissons.php">Boissons</a>
+        </div>
+    </div>
+
+</body>
+
+</html>
